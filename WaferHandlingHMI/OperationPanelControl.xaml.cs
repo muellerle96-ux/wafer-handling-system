afbe099cc9c5f52a844ccad5d9d4ffa4c5d985be
leonHSSM <l.mueller.c@stud.fh-sm.de>
2025-08-14 21:21:54 +0200

    implement the click event of the start button

--- WaferHandlingHMI/OperationPanelControl.xaml.cs
+++ WaferHandlingHMI/OperationPanelControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using WaferHandlingHMI.ViewModels;
 
 namespace WaferHandlingHMI
 {
@@ -7,9 +9,37 @@ namespace WaferHandlingHMI
     /// </summary>
     public partial class OperationPanelControl : UserControl
     {
+        // The button needs to start the operation with the robot view model, so it's needed as a dependency property
+        public static readonly DependencyProperty RobotViewModelProperty =
+            DependencyProperty.Register(
+                nameof(RobotViewModel),
+                typeof(RobotViewModel),
+                typeof(OperationPanelControl),
+                new PropertyMetadata(null));
+
+        public RobotViewModel RobotViewModel
+        {
+            get => (RobotViewModel)GetValue(RobotViewModelProperty);
+            set => SetValue(RobotViewModelProperty, value);
+        }
+
         public OperationPanelControl()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Start the transfer with the robot view model
+            RobotViewModel?.StartTransfer();
+            StopButton.IsEnabled = true;
+            StartButton.IsEnabled = false;
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopButton.IsEnabled = false;
+            StartButton.IsEnabled = true;
         }
     }
 }
